196a811e90484d70d6fddcc3c0dcb91d61cfe8d2
Ammar Yasser <70114537+3mmarYasser@users.noreply.github.com>
2022-12-22 16:05:53 +0200

    animation home

--- src/pages/Home/components/AnimateSlide/AnimationLine.home.tsx
+++ src/pages/Home/components/AnimateSlide/AnimationLine.home.tsx
@@ -14,11 +14,6 @@ import Styles  from "./../../Home.module.scss"
      "https://www.goodcv.com/images/cv/screenshots/thumbs/en/outstanding_10.png?v=1.0.1",
      "https://www.mycvstore.com/wp-content/uploads/2019/02/Professional-CV-Templat-2.jpg",
      "https://d.novoresume.com/images/doc/modern-cv-template.png",
-     "https://www.goodcv.com/images/cv/screenshots/thumbs/en/outstanding_10.png?v=1.0.1",
-     "https://www.mycvstore.com/wp-content/uploads/2019/02/Professional-CV-Templat-2.jpg",
-     "https://d.novoresume.com/images/doc/modern-cv-template.png",
-     "https://www.goodcv.com/images/cv/screenshots/thumbs/en/outstanding_10.png?v=1.0.1",
-     "https://www.mycvstore.com/wp-content/uploads/2019/02/Professional-CV-Templat-2.jpg",
  ]
 
  import React from 'react';
