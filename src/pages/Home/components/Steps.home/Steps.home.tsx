1e3666f5de175d09df5b25e5f82fff7724005fa1
Ammar Yasser <70114537+3mmarYasser@users.noreply.github.com>
2022-12-27 14:46:16 +0200

    steps

--- src/pages/Home/components/Steps.home/Steps.home.tsx
+++ src/pages/Home/components/Steps.home/Steps.home.tsx
@@ -5,35 +5,27 @@ const Steps:React.FC = () => {
     return (
         <>
                 <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-                        <div className="relative grid gap-8 row-gap-5 mb-8 md:row-gap-8 lg:grid-cols-3 sm:grid-cols-2">
-                                <div className="absolute inset-0 flex items-center justify-center sm:hidden lg:flex">
-                                        <div className="w-px h-full bg-gray-300 lg:w-full lg:h-px" />
+                        <div className="relative flex justify-between">
+                                <div className="absolute z-10 inset-0 flex items-center justify-center">
+                                        <div className="bg-gray-300 w-full h-px" />
                                 </div>
-                                <div className="p-5 duration-300 transform bg-white border rounded-full shadow-sm ">
-                                        <div className="flex items-center justify-between mb-2">
-                                                <p className="text-lg font-bold leading-5">Pick a template</p>
-                                                <p className="flex items-center justify-center w-6 h-6 font-bold rounded text-deep-purple-accent-400 bg-indigo-50">
-                                                        1
-                                                </p>
+                                <div className="z-10 flex flex-col items-center">
+                                        <div className="flex items-center justify-between mb-2 bg-base-100 w-64 h-64 border-2 rounded-full ">
                                         </div>
+                                        <h6>Pick a template</h6>
                                 </div>
-                                <div className="p-5 duration-300 transform bg-white border rounded-full shadow-sm ">
-                                        <div className="flex items-center justify-between mb-2">
-                                                <p className="text-lg font-bold leading-5">Enter all information about you </p>
-                                                <p className="flex items-center justify-center w-6 h-6 font-bold rounded text-deep-purple-accent-400 bg-indigo-50">
-                                                        2
-                                                </p>
+                                <div className="z-10 flex flex-col items-center">
+                                        <div className="flex items-center justify-between mb-2 bg-base-100 w-64 h-64 border-2 rounded-full ">
                                         </div>
+                                        <h6>Enter all information about you </h6>
+                                </div>
+                                <div className="z-10 flex flex-col items-center">
+                                        <div className="flex items-center justify-between mb-2 bg-base-100 w-64 h-64 border-2 rounded-full ">
+                                        </div>
+                                        <h6>download your resume without Sign Up</h6>
+                                </div>
 
-                                </div>
-                                <div className="p-5 duration-300 transform bg-white border rounded-full shadow-sm">
-                                        <div className="flex items-center justify-between mb-2">
-                                                <p className="text-lg font-bold leading-5">download your resume without Sign Up</p>
-                                                <p className="flex items-center justify-center w-6 h-6 font-bold rounded text-deep-purple-accent-400 bg-indigo-50">
-                                                        3
-                                                </p>
-                                        </div>
-                                </div>
+
                         </div>
                 </div>
         </>
